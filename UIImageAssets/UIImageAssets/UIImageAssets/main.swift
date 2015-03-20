62690ec2bbee604e6b432221bc7ded0ef352fb46
Colden Prime <coldenprime@gmail.com>
2015-03-20 16:45:58 -0400

    Add UIImageAssets Project

---
+++ UIImageAssets/UIImageAssets/UIImageAssets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UIImageAssets
+//
+//  Created by Colden Prime on 3/20/15.
+//  Copyright (c) 2015 IntrepidPursuits. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
