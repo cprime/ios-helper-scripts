76f346b1381db8c0cc59a736e06592cc0fd28256
Logan Wright <logan.william.wright@gmail.com>
2015-03-22 13:21:06 -0400

    working pod install

--- UIImageAssets/UIImageAssets/UIImageAssets/main.swift
+++ UIImageAssets/UIImageAssets/UIImageAssets/main.swift
@@ -38,6 +38,58 @@ Perhaps in addition, we should search for a category that exists with the same n
 */
 let toPath = currentDirectory
 
+//func shell(args: String...) -> Int32 {
+//    let task = NSTask()
+//    task.launchPath = "/usr/bin/env"
+//    task.arguments = args
+//    task.launch()
+//    task.waitUntilExit()
+//    return task.terminationStatus
+//}
+
+import Foundation
+
+//let task = NSTask()
+//task.launchPath = "/usr/local/bin/./git"
+//task.arguments = ["log", "--oneline"]
+//
+//let pipe = NSPipe()
+//task.standardOutput = pipe
+//task.launch()
+//
+//let data = pipe.fileHandleForReading.readDataToEndOfFile()
+//let output: String = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+//
+//print(output)
+//assert(output == "first-argument second-argument\n")
+
+func shell(input: String) -> (output: String, exitCode: Int32) {
+    let arguments = split(input, maxSplit: Int.max, allowEmptySlices: true) {
+        $0 == " "
+    }
+    
+    let task = NSTask()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = arguments
+    task.environment = [
+        "LC_ALL" : "en_US.UTF-8",
+        "HOME" : NSHomeDirectory(),
+        "PATH" : "/usr/bin"
+    ]
+    
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    task.launch()
+    task.waitUntilExit()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output: String = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+    
+    return (output, task.terminationStatus)
+}
+
+println(shell("pod install").output)
+
 
 // MARK: Image Scrape
 
@@ -46,20 +98,20 @@ Right now, this just finds all imageset folders in the current directory, perhap
 
 Temporary work around, navigate to that directory before generating file
 */
-var imageNames = ImageScraper.imageNames(parentDirectory: currentDirectory)
+//var imageNames = ImageScraper.imageNames(parentDirectory: currentDirectory)
 
 
 // MARK: File Write
 
-let imageWriter = ImageWriterObjC(
-    categoryName: categoryName,
-    rawImageNames: imageNames,
-    includeNonnull: nonnull
-)
-
-let success = imageWriter.writeToDirectoryPath(toPath)
-if success {
-    println("Successfully wrote to path: \(currentDirectory)")
-} else {
-    println("Process failed!")
-}
+//let imageWriter = ImageWriterObjC(
+//    categoryName: categoryName,
+//    rawImageNames: imageNames,
+//    includeNonnull: nonnull
+//)
+//
+//let success = imageWriter.writeToDirectoryPath(toPath)
+//if success {
+//    println("Successfully wrote to path: \(currentDirectory)")
+//} else {
+//    println("Process failed!")
+//}
